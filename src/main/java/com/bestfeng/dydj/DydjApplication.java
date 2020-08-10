44ea91731d160fef9ba8c4638d8520ef16f5650e
张辉 <zhanghui@qq.com>
2020-08-10 17:23:23 +0800

    fix bug

--- src/main/java/com/bestfeng/dydj/DydjApplication.java
+++ src/main/java/com/bestfeng/dydj/DydjApplication.java
@@ -1,13 +1,16 @@
 package com.bestfeng.dydj;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DydjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DydjApplication.class, args);
+		log.info("DydjApplication start success");
 	}
 
 }
