b03f57f8025b980a8025a064e7b60f0bc2456cfa
zhanghui <zhanghui>
2020-08-15 15:15:27 +0800

    add redis test

--- src/test/java/com/bestfeng/dydj/DydjApplicationTests.java
+++ src/test/java/com/bestfeng/dydj/DydjApplicationTests.java
@@ -1,6 +1,7 @@
 package com.bestfeng.dydj;
 
 import com.bestfeng.dydj.mbg.mapper.TestMapper;
+import com.bestfeng.dydj.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +11,15 @@ class DydjApplicationTests {
 
     @Autowired
     private TestMapper testMapper;
+    @Autowired
+    private RedisUtil redisUtil;
 
-//    @Test
-//    public void dataSourceTest(){
-//        com.bestfeng.dydj.mbg.model.Test test = testMapper.selectByPrimaryKey(1);
-//    }
+    @Test
+    public void dataSourceTest(){
+        com.bestfeng.dydj.mbg.model.Test test = testMapper.selectByPrimaryKey(1);
+    }
+
+    public void redisTest(){
+        redisUtil.set("test","1");
+    }
 }
