ef08d1200e857956d19b8cfd0581c51e00c1d2ac
张辉 <zhanghui@qq.com>
2020-09-16 17:51:38 +0800

    订单延迟队列

--- src/main/java/com/bestfeng/dydj/DelayTask/OrderUnPayDelayTask.java
+++ src/main/java/com/bestfeng/dydj/DelayTask/OrderUnPayDelayTask.java
@@ -6,6 +6,7 @@ import com.bestfeng.dydj.mbg.mapper.NoteOrderMapper;
 import com.bestfeng.dydj.mbg.model.NoteOrder;
 import com.bestfeng.dydj.service.CouponOrderService;
 import com.bestfeng.dydj.service.OrderService;
+import com.bestfeng.dydj.utils.FastJsons;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,8 +35,9 @@ public class OrderUnPayDelayTask implements Runnable {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void run() {
-        log.info("订单未支付订单关闭订单延迟消息消费 orderNo={}",orderNo);
+        log.info("订单未支付订单 关闭订单延迟消息消费 orderNo={}",orderNo);
         NoteOrder noteOrder = noteOrderMapper.selectObjByOrderNo(orderNo);
+        log.info("订单未支付订单 关闭订单延迟消息消费 查询出来的订单对象 obj={}", FastJsons.convertObjectToJSON(noteOrder));
         Optional.ofNullable(noteOrder).filter(order->order.getPaid() == OrderEnums.OrderPayStatusEnum.PAY_OFF.getCode()).ifPresent(
                 order->{
                     Integer couponId = noteOrder.getCouponid();
@@ -48,7 +50,7 @@ public class OrderUnPayDelayTask implements Runnable {
                         //卡券
                         couponOrderService.cancelUse(couponId);
                     }
-                    log.info("订单未支付订单关闭订单延迟消息消费 orderNo={} 关闭成功",orderNo);
+                    log.info("订单未支付订单 关闭订单延迟消息消费 orderNo={} 关闭成功",orderNo);
                 }
         );
     }
--- src/test/java/com/bestfeng/dydj/DydjApplicationTests.java
+++ src/test/java/com/bestfeng/dydj/DydjApplicationTests.java
@@ -1,12 +1,22 @@
 package com.bestfeng.dydj;
 
+import com.bestfeng.dydj.enums.NoteServiceStatusEnums;
+import com.bestfeng.dydj.enums.OrderEnums;
+import com.bestfeng.dydj.mbg.mapper.NoteOrderMapper;
 import com.bestfeng.dydj.mbg.mapper.TestMapper;
 import com.bestfeng.dydj.mbg.model.NoteOrder;
+import com.bestfeng.dydj.service.CouponOrderService;
+import com.bestfeng.dydj.service.OrderService;
 import com.bestfeng.dydj.service.impl.OrderServiceImpl;
 import com.bestfeng.dydj.utils.RedisUtil;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @SpringBootTest(classes = DydjApplication.class)
 class DydjApplicationTests {
@@ -25,4 +35,31 @@ class DydjApplicationTests {
 //    public void redisTest(){
 //        redisUtil.set("test","1");
 //    }
+    @Autowired
+    private NoteOrderMapper noteOrderMapper;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private CouponOrderService couponOrderService;
+
+    @Test
+    public void runTest(String orderNo) {
+        NoteOrder noteOrder = noteOrderMapper.selectObjByOrderNo(orderNo);
+        Optional.ofNullable(noteOrder).filter(order->order.getPaid() == OrderEnums.OrderPayStatusEnum.PAY_OFF.getCode()).ifPresent(
+                order->{
+                    Integer couponId = noteOrder.getCouponid();
+                    order.setStatus(OrderEnums.OrderStatusEnum.USER_CLOSE.getCode());
+                    //订单
+                    noteOrderMapper.updateByPrimaryKeySelective(order);
+                    //技师表
+                    orderService.noteServiceStatusHandel(noteOrder.getShopid(), NoteServiceStatusEnums.SERVICEABLE);
+                    if(null!=couponId && 0!=couponId){
+                        //卡券
+                        couponOrderService.cancelUse(couponId);
+                    }
+                    System.out.println("完成了");
+//                    log.info("订单未支付订单关闭订单延迟消息消费 orderNo={} 关闭成功",orderNo);
+                }
+        );
+    }
 }
